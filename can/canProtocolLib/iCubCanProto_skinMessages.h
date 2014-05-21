4a0bf1714044fd1ba4abc0fde6c74450a5164926
valegagge <valentina.gaggero@iit.it>
2014-05-21 14:57:01 +0200

    modified skin messages: added ICUBCANPROTO_POL_SK_CMD__ACC_GYRO_SETUP with value of 0x4f (already used) and changed values of ICUBCANPROTO_POL_SK_CMD__SET_TRIANG_CFG in 0x80. This modification should not affect any application.

--- can/canProtocolLib/iCubCanProto_skinMessages.h
+++ can/canProtocolLib/iCubCanProto_skinMessages.h
@@ -51,8 +51,8 @@ extern "C" {
 #define ICUBCANPROTO_POL_SK_CMD__TACT_SETUP         76 //0x4C
 #define ICUBCANPROTO_POL_SK_CMD__SET_BRD_CFG        77 //0x4D
 #define ICUBCANPROTO_POL_SK_CMD__TACT_SETUP2        78 //0x4E
-#define ICUBCANPROTO_POL_SK_CMD__SET_TRIANG_CFG     79 //0x4F
-
+#define ICUBCANPROTO_POL_SK_CMD__ACC_GYRO_SETUP     79 //0x4F
+#define ICUBCANPROTO_POL_SK_CMD__SET_TRIANG_CFG     80 //0x50
 
 // - declaration of public user-defined types ------------------------------------------------------------------------- 
 // empty-section
