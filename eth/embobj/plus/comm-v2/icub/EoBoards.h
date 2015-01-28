43449552edd1318ccb69effd83d00adf3f4f40b5
marcoaccame <marco.accame@iit.it>
2015-01-28 16:29:54 +0100

    now eObrd_cantype_t uses the same values as the can protocol

--- eth/embobj/plus/comm-v2/icub/EoBoards.h
+++ eth/embobj/plus/comm-v2/icub/EoBoards.h
@@ -66,17 +66,18 @@ typedef uint8_t eObrd_boardId_t;
 //    eobrd_skin                      = 6 	
 //} eObrd_types_t;
 
-
+// using the same values as in can protocol
 typedef enum
 {
-    eobrd_cantype_mc4               = 0,
-    eobrd_cantype_strain            = 1,
-    eobrd_cantype_1foc              = 2,
-    eobrd_cantype_mais              = 3,
-    eobrd_cantype_skin              = 4 	
+    eobrd_cantype_mc4               = 3,
+    eobrd_cantype_strain            = 6,
+    eobrd_cantype_1foc              = 8,
+    eobrd_cantype_mais              = 7,
+    eobrd_cantype_skin              = 5, 	
+    eobrd_cantype_unknown           = 255 
 } eObrd_cantype_t;
 
-enum { eobrd_cantypes_numberof = 5 };
+enum { eobrd_cantype_numberof = 5 };
 
 
 typedef struct                  // size is: 1+1+0 = 2
