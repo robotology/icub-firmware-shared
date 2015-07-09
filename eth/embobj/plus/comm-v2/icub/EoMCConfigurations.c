68d812cb4c79c97c41b543895950e04c3df09700
marco.accame <marco.accame@iit.it>
2015-07-09 09:33:23 +0200

    specified correct ordering of joints for mc4plus board which manages lips

--- eth/embobj/plus/comm-v2/icub/EoMCConfigurations.c
+++ eth/embobj/plus/comm-v2/icub/EoMCConfigurations.c
@@ -167,17 +167,17 @@ const eomcconfig_codeconfig_t eomcconfig_codeconfig_MC4PLUS[] =
     
     //board 15 - b9
     {EO_INIT(.value) eOmcconfig_value_MC4PLUS_b9,
-        // straight configuration (coherent with actual XML files)
-        EO_INIT(.jomos[0].actuator.local.type)  1, EO_INIT(.jomos[0].actuator.local.index) 0, EO_INIT(.jomos[0].encoder.etype)  2, EO_INIT(.jomos[0].encoder.index)  0,
-        EO_INIT(.jomos[1].actuator.local.type)  1, EO_INIT(.jomos[1].actuator.local.index) 1, EO_INIT(.jomos[1].encoder.etype)  2, EO_INIT(.jomos[1].encoder.index)  1,
-        EO_INIT(.jomos[2].actuator.local.type)  1, EO_INIT(.jomos[2].actuator.local.index) 2, EO_INIT(.jomos[2].encoder.etype)  2, EO_INIT(.jomos[2].encoder.index)  2,
-        EO_INIT(.jomos[3].actuator.local.type)  1, EO_INIT(.jomos[3].actuator.local.index) 3, EO_INIT(.jomos[3].encoder.etype)  2, EO_INIT(.jomos[3].encoder.index)  3
+//        // straight configuration (coherent with actual XML files)
+//        EO_INIT(.jomos[0].actuator.local.type)  1, EO_INIT(.jomos[0].actuator.local.index) 0, EO_INIT(.jomos[0].encoder.etype)  2, EO_INIT(.jomos[0].encoder.index)  0,
+//        EO_INIT(.jomos[1].actuator.local.type)  1, EO_INIT(.jomos[1].actuator.local.index) 1, EO_INIT(.jomos[1].encoder.etype)  2, EO_INIT(.jomos[1].encoder.index)  1,
+//        EO_INIT(.jomos[2].actuator.local.type)  1, EO_INIT(.jomos[2].actuator.local.index) 2, EO_INIT(.jomos[2].encoder.etype)  2, EO_INIT(.jomos[2].encoder.index)  2,
+//        EO_INIT(.jomos[3].actuator.local.type)  1, EO_INIT(.jomos[3].actuator.local.index) 3, EO_INIT(.jomos[3].encoder.etype)  2, EO_INIT(.jomos[3].encoder.index)  3
         
         // ordered config
-//        EO_INIT(.jomos[0].actuator.local.type)  1, EO_INIT(.jomos[0].actuator.local.index) 1, EO_INIT(.jomos[0].encoder.etype)  2, EO_INIT(.jomos[0].encoder.index)  1, // j0-> lip-high, motor1, incremental-encoder
-//        EO_INIT(.jomos[1].actuator.local.type)  1, EO_INIT(.jomos[1].actuator.local.index) 3, EO_INIT(.jomos[1].encoder.etype)  2, EO_INIT(.jomos[1].encoder.index)  3, // j1-> lip-bottom, motor3, incremental-encoder
-//        EO_INIT(.jomos[2].actuator.local.type)  1, EO_INIT(.jomos[2].actuator.local.index) 0, EO_INIT(.jomos[2].encoder.etype)  2, EO_INIT(.jomos[2].encoder.index)  0, // j2-> lip-right, motor0, incremental-encoder
-//        EO_INIT(.jomos[3].actuator.local.type)  1, EO_INIT(.jomos[3].actuator.local.index) 2, EO_INIT(.jomos[3].encoder.etype)  2, EO_INIT(.jomos[3].encoder.index)  2 // j3-> lip-left, motor2, incremental-encoder
+        EO_INIT(.jomos[0].actuator.local.type)  1, EO_INIT(.jomos[0].actuator.local.index) 1, EO_INIT(.jomos[0].encoder.etype)  2, EO_INIT(.jomos[0].encoder.index)  1, // j0-> lip-high, motor1, incremental-encoder
+        EO_INIT(.jomos[1].actuator.local.type)  1, EO_INIT(.jomos[1].actuator.local.index) 3, EO_INIT(.jomos[1].encoder.etype)  2, EO_INIT(.jomos[1].encoder.index)  3, // j1-> lip-bottom, motor3, incremental-encoder
+        EO_INIT(.jomos[2].actuator.local.type)  1, EO_INIT(.jomos[2].actuator.local.index) 0, EO_INIT(.jomos[2].encoder.etype)  2, EO_INIT(.jomos[2].encoder.index)  0, // j2-> lip-right, motor0, incremental-encoder
+        EO_INIT(.jomos[3].actuator.local.type)  1, EO_INIT(.jomos[3].actuator.local.index) 2, EO_INIT(.jomos[3].encoder.etype)  2, EO_INIT(.jomos[3].encoder.index)  2 // j3-> lip-left, motor2, incremental-encoder
     },
     
 }; EO_VERIFYsizeof(eomcconfig_codeconfig_MC4PLUS, eOmcconfig_value_MC4PLUS_numberof*sizeof(const eomcconfig_codeconfig_t)); 
