f48849b77e4b96e0f32e8b8e9719274b70d68b22
marco.accame <marco.accame@iit.it>
2016-07-06 10:57:19 +0200

    added uprot_RES_ERR_TRYAGAIN

--- eth/embobj/plus/comm-v2/icub/EoUpdaterProtocol.h
+++ eth/embobj/plus/comm-v2/icub/EoUpdaterProtocol.h
@@ -125,7 +125,8 @@ typedef enum
     uprot_RES_ERR_LOST          = 3,
     uprot_RES_ERR_UNK           = 4,
     uprot_RES_ERR_UNSUPPORTED   = 5,
-    uprot_RES_ERR_FAILED        = 6       
+    uprot_RES_ERR_FAILED        = 6,
+    uprot_RES_ERR_TRYAGAIN      = 7       
 } eOuprot_result_t;
 
 
