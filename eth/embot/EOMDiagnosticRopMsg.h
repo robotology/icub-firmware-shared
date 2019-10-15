9d292dde78b08398defa9300d5a4185d1f011fac
triccyx <triccyx@gmail.com>
2019-10-15 15:37:12 +0200

    fix

--- eth/embot/EOMDiagnosticRopMsg.h
+++ eth/embot/EOMDiagnosticRopMsg.h
@@ -42,14 +42,7 @@ class EOMDiagnosticRopMsg
 public:
     struct Info
     {
-        uint16_t code_{(uint16_t)DiagnosticRopCode::empty};
-        uint16_t severity_{(uint16_t)DiagnosticRopSeverity::empty};
-        uint16_t param2_{0};
-        uint16_t param3_{0};
-        uint16_t param4_{0};
-        uint16_t param5_{0};
-        uint16_t param6_{0};
-        uint16_t param7_{0};
+        std::array<uint16_t,8> param_{(uint16_t)DiagnosticRopCode::empty,(uint16_t)DiagnosticRopSeverity::empty,0,0,0,0,0,0};
         uint64_t time_{0};
     };
     EO_VERIFYsizeof(Info, 24)
@@ -71,7 +64,6 @@ public:
     DiagnosticRopCode getCode();
     DiagnosticRopSeverity getSeverity();
 
-private:
     Info data_;
 };
 
@@ -82,12 +74,12 @@ inline void EOMDiagnosticRopMsg::reset()
 
 inline DiagnosticRopCode EOMDiagnosticRopMsg::getCode()
 {
-    return (DiagnosticRopCode)(data_.code_);
+    return (DiagnosticRopCode)(data_.param_[0]);
 }
 
 inline DiagnosticRopSeverity EOMDiagnosticRopMsg::getSeverity()
 {
-    return static_cast<DiagnosticRopSeverity>(data_.severity_);
+    return static_cast<DiagnosticRopSeverity>(data_.param_[1]);
 }
 
 inline EOMDiagnosticRopMsg::EOMDiagnosticRopMsg(void *data, uint8_t size)
@@ -126,37 +118,37 @@ inline void EOMDiagnosticRopMsg::dump(const std::map<DiagnosticRopSeverity,std::
     std::string codeStr{"unknown"};
     if(ropCode)
     {
-        if(ropCode->find((DiagnosticRopCode)data_.code_)!=ropCode->end())
-            codeStr=ropCode->at((DiagnosticRopCode)data_.code_);
+        if(ropCode->find((DiagnosticRopCode)data_.param_[0])!=ropCode->end())
+            codeStr=ropCode->at((DiagnosticRopCode)data_.param_[0]);
     }      
     std::string severityStr{"unknown"};
     if(ropSeverity)
     {
-        if(ropSeverity->find((DiagnosticRopSeverity)data_.severity_)!=ropSeverity->end())
-            severityStr=ropSeverity->at((DiagnosticRopSeverity)data_.severity_);    
+        if(ropSeverity->find((DiagnosticRopSeverity)data_.param_[1])!=ropSeverity->end())
+            severityStr=ropSeverity->at((DiagnosticRopSeverity)data_.param_[1]);    
     }
         
     std::string stringStr{"unknown"};
     if(ropString)
     {
-        if(ropString->find((DiagnosticRopString)data_.param2_)!=ropString->end())
-            stringStr=ropString->at((DiagnosticRopString)data_.param2_);        
+        if(ropString->find((DiagnosticRopString)data_.param_[2])!=ropString->end())
+            stringStr=ropString->at((DiagnosticRopString)data_.param_[2]);        
     }
 
-    if((DiagnosticRopCode)data_.code_==DiagnosticRopCode::empty)
+    if((DiagnosticRopCode)data_.param_[0]==DiagnosticRopCode::empty)
         return;
-    
+
     std::cout << "------ROP";
     std::cout << "code:" <<std::left <<std::setw (15)<<codeStr << " ";
     std::cout << "severity:" <<std::left<<std::setw (15)<<severityStr << " ";
     std::cout << "param2:" <<std::left<<std::setw (15)<<stringStr << " ";
-    std::cout << "param3:" << data_.param3_ << " ";
-    std::cout << "param4:" << data_.param4_ << " ";
-    std::cout << "param5:" << data_.param5_ << " ";
-    std::cout << "param6:" << data_.param6_ << " ";
-    std::cout << "param7:" << data_.param7_ << " ";
+    std::cout << "param3:" << data_.param_[3] << " ";
+    std::cout << "param4:" << data_.param_[4] << " ";
+    std::cout << "param5:" << data_.param_[5] << " ";
+    std::cout << "param6:" << data_.param_[6] << " ";
+    std::cout << "param7:" << data_.param_[7] << " ";
     std::cout << "time:" << data_.time_ << " ";
-    std::cout << std::endl;
+    std::cout << std::endl;    
 };
 
 #endif // include-guard
