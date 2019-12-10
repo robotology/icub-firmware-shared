1aa289c79abe22cc5d4e2f7877291fd9fcc681af
marco.accame <marco.accame@iit.it>
2019-12-10 19:10:28 +0100

    moved eoprot_version_mc to 1.22 and icub_firmware_shared to 4.0.7

--- eth/embobj/plus/comm-v2/protocol/api/EoProtocolMC.h
+++ eth/embobj/plus/comm-v2/protocol/api/EoProtocolMC.h
@@ -57,7 +57,7 @@ extern "C" {
 // - declaration of public user-defined types ------------------------------------------------------------------------- 
 
 
-enum { eoprot_version_mc_major = 1, eoprot_version_mc_minor = 21 };
+enum { eoprot_version_mc_major = 1, eoprot_version_mc_minor = 22 };
 
 enum { eoprot_entities_mc_numberof = eomc_entities_numberof };
 
