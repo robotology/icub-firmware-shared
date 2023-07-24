9fc93471cbbbf62080e7bdcd6e551605753aec93
tumme <davide.tome@iit.it>
2023-07-24 16:59:47 +0200

    changes for [200~parallalerized motor drivers

--- eth/embobj/plus/comm-v2/icub/EoBoards.h
+++ eth/embobj/plus/comm-v2/icub/EoBoards.h
@@ -314,12 +314,15 @@ typedef enum
     eobrd_conn_J31      = 32,
     eobrd_conn_J32      = 33,
     eobrd_conn_J33      = 34,
+    eobrd_conn_P3P2     = 35,
+    eobrd_conn_P4P5     = 36,
+
     
     eobrd_conn_none     = 0,
     eobrd_conn_unknown  = 255
 } eObrd_connector_t;
 
-enum { eobrd_connectors_numberof = 34 };
+enum { eobrd_connectors_numberof = 36 };
 
 
 typedef enum
@@ -339,6 +342,8 @@ typedef enum
     eobrd_port_mc4plusP3            = 0,        // PWM & QUADENC: hal_motor1, hal_quad_enc1
     eobrd_port_mc4plusP4            = 2,        // PWM & QUADENC: hal_motor3, hal_quad_enc3
     eobrd_port_mc4plusP5            = 3,        // PWM & QUADENC: hal_motor4, hal_quad_enc4
+    eobrd_port_mc4plusP3P2          = 4,        // PWM & QUADENC: hal_motor1, hal_quad_enc1 parrallelized driver
+    eobrd_port_mc4plusP4P5          = 5,        // PWM & QUADENC: hal_motor3, hal_quad_enc3 parrallelized driver
     eobrd_port_mc4plusP10           = 0,        // SPI encoder: hal_encoder1
     eobrd_port_mc4plusP11           = 1,        // SPI encoder: hal_encoder2
     
